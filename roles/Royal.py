c68349d6eeb5403984fc221436a07ae3e4c976dc
damian-bots <tosu0791@gmail.com>
2024-12-14 15:15:15 +0530

    Update Royal.py

--- roles/Royal.py
+++ roles/Royal.py
@@ -3,7 +3,6 @@ import strings as s
 
 class Royal(Role):
     """A member of the Royal Games. The main role, has no power other than to vote."""
-    icon = s.royal_icon
     team = 'Good'
     name = s.royal_name
 
