e1ee6069e981e58feecd0e4d3a97f254c206bbfc
damian-bots <tosu0791@gmail.com>
2024-12-15 02:03:06 +0530

    Update Royal.py

--- roles/Royal.py
+++ roles/Royal.py
@@ -4,7 +4,7 @@ import strings as s
 class Royal(Role):
     """A member of the Royal Games. The main role, has no power other than to vote."""
     team = 'Good'
-    name = s.royal_name
+    name = s.Royal_Name
 
     def __init__(self, player):
         super().__init__(player)
