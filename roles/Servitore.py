505bcd6fd4a8b3453d4e1f1b2ca428a3163e30fd
damian-bots <tosu0791@gmail.com>
2024-12-15 02:03:21 +0530

    Update Servitore.py

--- roles/Servitore.py
+++ roles/Servitore.py
@@ -6,7 +6,7 @@ class Servitore(Role):
     """The Chaos minion is the Chaos Lord's underling.The Chaos minion is the Chaos Lord's underling.
 If there are no Chaos Lords in the game, he becomes the Chaos Lord.If there are no Chaos Lords in the game, he becomes the Chaos Lord."""
     team = 'Chaos'
-    name = s.chaos_servant_name
+    name = s.Chaos_Servant_name
     powerdesc = s.Chaos_Servant_power_description
 
     def __repr__(self) -> str:
